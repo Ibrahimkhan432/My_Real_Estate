f542fc45ae1c53cc1b9680e1823fd1ec3a96bc1c
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-16 13:17:23 +0500

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import deotenv from "dotenv"
+deotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("connected to database")
+    }).catch((error) => {
+        console.log("failed to connect to database", error)
+    })
 
 const app = express();
 
