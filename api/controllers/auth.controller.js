732ae22113de7e37ad3e5dd5a66ad5cf152380f4
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2025-08-21 18:59:20 +0500

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 export const signUp = async (req, res) => {
   const { userName, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -12,4 +15,32 @@ export const signUp = async (req, res) => {
       .status(500)
       .json({ error: "User creation failed", message: error.message });
   }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const isPasswordValid = bcryptjs.compareSync(password, validUser.password);
+    if (!isPasswordValid) {
+      return next(errorHandler(401, "Wrong Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    // Remove password from the user object before sending response
+    validUser.password = undefined;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(validUser);
+  } catch (error) {
+    res.status(500).json({ error: "Login failed", message: error.message });
+  }
+  next();
 };
